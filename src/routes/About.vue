2b817008afc65444d4cef8d822454d9a78f93caf
seadonggyun4 <seadonggyun@gmail.com>
2022-03-03 15:19:44 +0900

    💄 about 컴포넌트 작성

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,98 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="photo">
+      <!-- 로딩 애니메이션 -->
+      <Loader
+        v-if="imageLoading"
+        :absolute="true" />
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
+
+
+<script>
+import Loader from '~/components/Loader.vue'
+
+export default {
+  data(){
+    return{
+      imageLoading: true
+    }
+  },
+  components:{
+    Loader
+  },
+  computed:{
+    image(){
+      //store -> state -> about -> image데이터
+      return this.$store.state.about.image
+    },
+    name(){
+      //store -> state -> about -> name데이터
+      return this.$store.state.about.name
+    },
+    email(){
+      //store -> state -> about -> email데이터
+      return this.$store.state.about.email
+    },
+    blog(){
+      //store -> state -> about -> blog데이터
+      return this.$store.state.about.blog
+    },
+    phone(){
+      //store -> state -> about -> phone데이터
+      return this.$store.state.about.phone
+    }
+  },
+  methods: {
+    async init(){
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  },
+  mounted(){
+    this.init()
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '~/scss/main.scss';
+
+.about{
+  text-align: center;
+
+  .photo{
+    position: relative;
+    margin: 40px auto 20px;
+    padding: 30px;
+    width: 250px;
+    height: 250px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    background-color: $gray-200;
+    overflow: hidden;
+
+    img{
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+
+  .name{
+    margin-bottom: 20px;
+    font-family: 'Oswald',sans-serif;
+    font-size: 40px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,12 @@
+export default{
+  namespaced: true,
+  state: () => {
+    return{
+      name: 'DongGyun',
+      email: 'seadonggyun@gmail.com',
+      blog:'https://github.com/seadonggyun4',
+      phone: '+82-10-2889-4251',
+      image: 'https://donggyunportfolio.netlify.app/asset/img/profile-photo.jpg'
+    }
+  }
+}
