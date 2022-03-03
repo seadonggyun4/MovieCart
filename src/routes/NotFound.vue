db0869550615e0ee4b7d8fef29d62d57f9ff926f
seadonggyun4 <seadonggyun@gmail.com>
2022-03-03 15:44:11 +0900

    💄 404 Page 생성

---
+++ src/routes/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="not-found">
+    <div class="status">
+      404
+    </div>
+    <div class="message">
+      Page Not Found!
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/scss/main.scss';
+
+.not-found{
+  margin: auto;
+  padding: 100px 20px;
+  width: 500px;
+  font-family: 'Oswald',sans-serif;
+  line-height: 1.2;
+  text-align: center;
+  border: 10px solid $primary;
+  border-radius: 50%;
+
+  .status{
+    font-size: 160px;
+    color: $primary;
+  }
+
+  .message{
+    font-size: 50px;
+  }
+}
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './Home.vue'
 import About from './About.vue'
 import Movie from './Movie.vue'
+import NotFound from './NotFound.vue'
 
 export default createRouter({
   // Hash or History -> 모드선택
@@ -19,9 +20,16 @@ export default createRouter({
       path: '/about',
       component: About
     },{
-      // 파라메터 값 id
+      // id 파라메터 -> id 주소가 입력된다.
+      // id 파라메터 와 같은 파라메터 이름은 직관적으로 프로그래머 본인이 설정한다.
       path: '/movie/:id',
       component: Movie
+    },{
+      // pathMatch 파라메터 -> 정규표현식 (.*)를 검사해 루트경로에 작성하는 모든 문자와 일치시킨다.
+      // 정규 표현식을 통해 일치된 모든 주소는 notFound 컴포넌트로 보낸다
+      // 결국 / , /about, /movie/:id 를 제외한 모든 주소는 notFound 컴포넌트로 가는 구조이다.
+      path: '/:pathMatch(.*)',
+      component: NotFound
     }
   ]
 })
